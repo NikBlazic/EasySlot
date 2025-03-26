23c65bd8fdc84491d186b13cb16216b250c33441
NikBlazic <nik.blazic07@gmail.com>
2025-03-26 01:53:46 +0100

    inital commit

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route("/")
+def quote():
+    return render_template("homepage.html")
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+
+app.run(debug = True)
